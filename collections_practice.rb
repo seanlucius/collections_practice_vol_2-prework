d6cace808cfb619a62c9deff9c732d5224749711
Sean Lucius <seanlucius22@icloud.com>
2019-02-07 01:14:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -30,7 +30,8 @@ def organize_schools(collection)
   org_hash = {}
   collection.each do |school, value_hash|
     value_hash.each do |k, v|
-      org_hash[v] || = []
+      org_hash[v] ||= []
+      org_hash[v] << school
     end 
   end 
   org_hash
